ac0476abb4cf35554966b4d8d8c9cb2cfd2d4d0c
Tom Oram <me@tomoram.io>
2020-05-09 22:34:55 +0100

    Add clap to build CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new("specdown");
+
+    let spec_file = Arg::with_name("spec-file")
+        .long("spec-file")
+        .takes_value(true)
+        .help("The spec file to run")
+        .required(true);
+
+    let output_file = Arg::with_name("output-file")
+        .long("output-file")
+        .takes_value(true)
+        .help("The generated output file")
+        .required(true);
+
+    let app = app.arg(spec_file);
+    let app = app.arg(output_file);
+
+    let _matches = app.get_matches();
+
+    // let name = matches.value_of("name")
+    //     .expect("This can't be None, we said it was required");
+
+    // println!("Hello, {}!", name);
 }
---
+++ tests/command_test.rs
@@ -0,0 +1,43 @@
+use assert_cmd::Command;
+use indoc::indoc;
+
+#[test]
+fn test_displays_help() {
+    Command::cargo_bin("specdown").unwrap()
+        .arg("--help")
+        .assert()
+        .success()
+        .stdout(
+            indoc!("specdown 
+
+            USAGE:
+                specdown --output-file <output-file> --spec-file <spec-file>
+
+            FLAGS:
+                -h, --help       Prints help information
+                -V, --version    Prints version information
+
+            OPTIONS:
+                    --output-file <output-file>    The generated output file
+                    --spec-file <spec-file>        The spec file to run
+            ")
+        );
+}
+
+#[test]
+fn test_displays_error_when_required_args_are_missing() {
+    Command::cargo_bin("specdown").unwrap()
+        .assert()
+        .failure()
+        .stderr(
+            indoc!("error: The following required arguments were not provided:
+                --output-file <output-file>
+                --spec-file <spec-file>
+            
+            USAGE:
+                specdown --output-file <output-file> --spec-file <spec-file>
+            
+            For more information try --help
+            ")
+        );
+}
