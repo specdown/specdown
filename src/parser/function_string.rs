168d4552e489aaf415dfeb7e8063fc5667b0de7e
Tom Oram <me@tomoram.io>
2020-05-16 18:38:46 +0100

    Re-enable commented out tests

--- src/parser/function_string.rs
+++ src/parser/function_string.rs
@@ -83,271 +83,289 @@ fn token_value(input: &str) -> IResult<&str, ArgumentValue> {
     })(input)
 }
 
-// mod tests {
-//     #[cfg(test)]
-//     use super::*;
+mod tests {
+    #[cfg(test)]
+    use super::*;
 
-//     mod parse {
-//         #[cfg(test)]
-//         use super::*;
+    mod parse {
+        #[cfg(test)]
+        use super::*;
 
-//         #[test]
-//         fn succeeds_when_function_has_no_args() {
-//             assert_eq!(
-//                 parse("func(), more"),
-//                 Ok((
-//                     ", more",
-//                     Function {
-//                         name: "func",
-//                         arguments: HashMap::new()
-//                     }
-//                 ))
-//             )
-//         }
+        #[test]
+        fn succeeds_when_function_has_no_args() {
+            assert_eq!(
+                parse("func(), more"),
+                Ok((
+                    ", more",
+                    Function {
+                        name: "func".to_string(),
+                        arguments: HashMap::new()
+                    }
+                ))
+            )
+        }
 
-//         #[test]
-//         fn succeeds_when_function_has_args() {
-//             assert_eq!(
-//                 parse("funcy(arg=\"hi\")"),
-//                 Ok((
-//                     "",
-//                     Function {
-//                         name: "funcy",
-//                         arguments: [("arg", ArgumentValue::String("hi"))]
-//                             .iter()
-//                             .cloned()
-//                             .collect()
-//                     }
-//                 ))
-//             )
-//         }
+        #[test]
+        fn succeeds_when_function_has_args() {
+            assert_eq!(
+                parse("funcy(arg=\"hi\")"),
+                Ok((
+                    "",
+                    Function {
+                        name: "funcy".to_string(),
+                        arguments: [("arg".to_string(), ArgumentValue::String("hi".to_string()))]
+                            .iter()
+                            .cloned()
+                            .collect()
+                    }
+                ))
+            )
+        }
 
-//         #[test]
-//         fn succeeds_when_no_arguments_are_provided() {
-//             let expected_fn = Function {
-//                 name: "fn",
-//                 arguments: HashMap::new(),
-//             };
-//             assert_eq!(parse("fn()"), Ok(("", expected_fn)));
-//         }
+        #[test]
+        fn succeeds_when_no_arguments_are_provided() {
+            let expected_fn = Function {
+                name: "fn".to_string(),
+                arguments: HashMap::new(),
+            };
+            assert_eq!(parse("fn()"), Ok(("", expected_fn)));
+        }
 
-//         #[test]
-//         fn succeeds_when_one_argument_is_provided() {
-//             let expected_fn = Function {
-//                 name: "fn",
-//                 arguments: [("arg", ArgumentValue::String("abc"))]
-//                     .iter()
-//                     .cloned()
-//                     .collect(),
-//             };
+        #[test]
+        fn succeeds_when_one_argument_is_provided() {
+            let expected_fn = Function {
+                name: "fn".to_string(),
+                arguments: [("arg".to_string(), ArgumentValue::String("abc".to_string()))]
+                    .iter()
+                    .cloned()
+                    .collect(),
+            };
 
-//             assert_eq!(parse("fn(arg=\"abc\")"), Ok(("", expected_fn)));
-//         }
+            assert_eq!(parse("fn(arg=\"abc\")"), Ok(("", expected_fn)));
+        }
 
-//         #[test]
-//         fn succeeds_when_multiple_arguments_are_provided() {
-//             let expected_fn = Function {
-//                 name: "fn",
-//                 arguments: [
-//                     ("arg1", ArgumentValue::Token("abc")),
-//                     ("arg2", ArgumentValue::String("def")),
-//                 ]
-//                 .iter()
-//                 .cloned()
-//                 .collect(),
-//             };
+        #[test]
+        fn succeeds_when_multiple_arguments_are_provided() {
+            let expected_fn = Function {
+                name: "fn".to_string(),
+                arguments: [
+                    ("arg1".to_string(), ArgumentValue::Token("abc".to_string())),
+                    ("arg2".to_string(), ArgumentValue::String("def".to_string())),
+                ]
+                .iter()
+                .cloned()
+                .collect(),
+            };
 
-//             assert_eq!(parse("fn(arg1=abc,arg2=\"def\")"), Ok(("", expected_fn)));
-//         }
+            assert_eq!(parse("fn(arg1=abc,arg2=\"def\")"), Ok(("", expected_fn)));
+        }
 
-//         #[test]
-//         fn succeeds_when_multiple_arguments_are_provided_with_spaces() {
-//             let expected_fn = Function {
-//                 name: "fn",
-//                 arguments: [
-//                     ("arg1", ArgumentValue::Token("xxx")),
-//                     ("arg2", ArgumentValue::String("123")),
-//                 ]
-//                 .iter()
-//                 .cloned()
-//                 .collect(),
-//             };
+        #[test]
+        fn succeeds_when_multiple_arguments_are_provided_with_spaces() {
+            let expected_fn = Function {
+                name: "fn".to_string(),
+                arguments: [
+                    ("arg1".to_string(), ArgumentValue::Token("xxx".to_string())),
+                    ("arg2".to_string(), ArgumentValue::String("123".to_string())),
+                ]
+                .iter()
+                .cloned()
+                .collect(),
+            };
 
-//             assert_eq!(parse("fn(arg1=xxx , arg2=\"123\")"), Ok(("", expected_fn)));
-//         }
-//     }
+            assert_eq!(parse("fn(arg1=xxx , arg2=\"123\")"), Ok(("", expected_fn)));
+        }
+    }
 
-//     mod argument_list {
-//         #[cfg(test)]
-//         use super::*;
+    mod argument_list {
+        #[cfg(test)]
+        use super::*;
 
-//         #[test]
-//         fn succeeds_when_no_arguments_are_provided() {
-//             let expected_args: HashMap<&str, ArgumentValue> = [].iter().cloned().collect();
-//             assert_eq!(argument_list("()"), Ok(("", expected_args)));
-//         }
+        #[test]
+        fn succeeds_when_no_arguments_are_provided() {
+            let expected_args: HashMap<String, ArgumentValue> = [].iter().cloned().collect();
+            assert_eq!(argument_list("()"), Ok(("", expected_args)));
+        }
 
-//         #[test]
-//         fn succeeds_when_one_argument_is_provided() {
-//             let expected_args: HashMap<&str, ArgumentValue> =
-//                 [("arg", ArgumentValue::String("abc"))]
-//                     .iter()
-//                     .cloned()
-//                     .collect();
+        #[test]
+        fn succeeds_when_one_argument_is_provided() {
+            let expected_args: HashMap<String, ArgumentValue> =
+                [("arg".to_string(), ArgumentValue::String("abc".to_string()))]
+                    .iter()
+                    .cloned()
+                    .collect();
 
-//             assert_eq!(argument_list("(arg=\"abc\")"), Ok(("", expected_args)));
-//         }
+            assert_eq!(argument_list("(arg=\"abc\")"), Ok(("", expected_args)));
+        }
 
-//         #[test]
-//         fn succeeds_when_multiple_arguments_are_provided() {
-//             let expected_args: HashMap<&str, ArgumentValue> = [
-//                 ("arg1", ArgumentValue::Token("abc")),
-//                 ("arg2", ArgumentValue::String("def")),
-//             ]
-//             .iter()
-//             .cloned()
-//             .collect();
+        #[test]
+        fn succeeds_when_multiple_arguments_are_provided() {
+            let expected_args: HashMap<String, ArgumentValue> = [
+                ("arg1".to_string(), ArgumentValue::Token("abc".to_string())),
+                ("arg2".to_string(), ArgumentValue::String("def".to_string())),
+            ]
+            .iter()
+            .cloned()
+            .collect();
 
-//             assert_eq!(
-//                 argument_list("(arg1=abc,arg2=\"def\")"),
-//                 Ok(("", expected_args))
-//             );
-//         }
+            assert_eq!(
+                argument_list("(arg1=abc,arg2=\"def\")"),
+                Ok(("", expected_args))
+            );
+        }
 
-//         #[test]
-//         fn succeeds_when_multiple_arguments_are_provided_with_spaces() {
-//             let expected_args: HashMap<&str, ArgumentValue> = [
-//                 ("arg1", ArgumentValue::Token("xxx")),
-//                 ("arg2", ArgumentValue::String("123")),
-//             ]
-//             .iter()
-//             .cloned()
-//             .collect();
-//             assert_eq!(
-//                 argument_list("(arg1=xxx , arg2=\"123\")"),
-//                 Ok(("", expected_args))
-//             );
-//         }
-//     }
+        #[test]
+        fn succeeds_when_multiple_arguments_are_provided_with_spaces() {
+            let expected_args: HashMap<String, ArgumentValue> = [
+                ("arg1".to_string(), ArgumentValue::Token("xxx".to_string())),
+                ("arg2".to_string(), ArgumentValue::String("123".to_string())),
+            ]
+            .iter()
+            .cloned()
+            .collect();
+            assert_eq!(
+                argument_list("(arg1=xxx , arg2=\"123\")"),
+                Ok(("", expected_args))
+            );
+        }
+    }
 
-//     mod argument {
-//         #[cfg(test)]
-//         use super::*;
+    mod argument {
+        #[cfg(test)]
+        use super::*;
 
-//         #[test]
-//         fn fails_when_name_starts_with_a_digit() {
-//             assert_eq!(
-//                 argument("1arg=\"value\",more..."),
-//                 Err(nom::Err::Error(nom::error_position!(
-//                     "1arg=\"value\",more...",
-//                     nom::error::ErrorKind::Alpha
-//                 )))
-//             );
-//         }
+        #[test]
+        fn fails_when_name_starts_with_a_digit() {
+            assert_eq!(
+                argument("1arg=\"value\",more..."),
+                Err(nom::Err::Error(nom::error_position!(
+                    "1arg=\"value\",more...",
+                    nom::error::ErrorKind::Alpha
+                )))
+            );
+        }
 
-//         #[test]
-//         fn succeeds_when_single_alpha_char_name() {
-//             assert_eq!(
-//                 argument("a=\"value\",more..."),
-//                 Ok((",more...", ("a", ArgumentValue::String("value"))))
-//             );
-//         }
+        #[test]
+        fn succeeds_when_single_alpha_char_name() {
+            assert_eq!(
+                argument("a=\"value\",more..."),
+                Ok((
+                    ",more...",
+                    ("a", ArgumentValue::String("value".to_string()))
+                ))
+            );
+        }
 
-//         #[test]
-//         fn succeeds_when_alpha_numeric_name() {
-//             assert_eq!(
-//                 argument("arg1=\"value\",more..."),
-//                 Ok((",more...", ("arg1", ArgumentValue::String("value"))))
-//             );
-//         }
+        #[test]
+        fn succeeds_when_alpha_numeric_name() {
+            assert_eq!(
+                argument("arg1=\"value\",more..."),
+                Ok((
+                    ",more...",
+                    ("arg1", ArgumentValue::String("value".to_string()))
+                ))
+            );
+        }
 
-//         #[test]
-//         fn succeds_with_token_argument() {
-//             assert_eq!(
-//                 argument("arg=token,more..."),
-//                 Ok((",more...", ("arg", ArgumentValue::Token("token"))))
-//             );
-//         }
+        #[test]
+        fn succeds_with_token_argument() {
+            assert_eq!(
+                argument("arg=token,more..."),
+                Ok((
+                    ",more...",
+                    ("arg", ArgumentValue::Token("token".to_string()))
+                ))
+            );
+        }
 
-//         #[test]
-//         fn succeeds_when_arg_contains_underscore() {
-//             assert_eq!(
-//                 argument("arg_name=\"value\",more..."),
-//                 Ok((",more...", ("arg_name", ArgumentValue::String("value"))))
-//             );
-//         }
+        #[test]
+        fn succeeds_when_arg_contains_underscore() {
+            assert_eq!(
+                argument("arg_name=\"value\",more..."),
+                Ok((
+                    ",more...",
+                    ("arg_name", ArgumentValue::String("value".to_string()))
+                ))
+            );
+        }
 
-//         #[test]
-//         fn succeds_with_spaces_around_equals() {
-//             assert_eq!(
-//                 argument("arg  =  token rest"),
-//                 Ok((" rest", ("arg", ArgumentValue::Token("token"))))
-//             );
-//         }
-//     }
+        #[test]
+        fn succeds_with_spaces_around_equals() {
+            assert_eq!(
+                argument("arg  =  token rest"),
+                Ok((" rest", ("arg", ArgumentValue::Token("token".to_string()))))
+            );
+        }
+    }
 
-//     mod argument_value {
-//         #[cfg(test)]
-//         use super::*;
+    mod argument_value {
+        #[cfg(test)]
+        use super::*;
 
-//         #[test]
-//         fn succeeds_when_there_is_a_remainder() {
-//             assert_eq!(
-//                 argument_value("\"string\" rest"),
-//                 Ok((" rest", ArgumentValue::String("string")))
-//             );
-//         }
+        #[test]
+        fn succeeds_when_there_is_a_remainder() {
+            assert_eq!(
+                argument_value("\"string\" rest"),
+                Ok((" rest", ArgumentValue::String("string".to_string())))
+            );
+        }
 
-//         // #[test]
-//         // fn succeeds_when_there_is_no_remainder() {
-//         //     let result = argument_value("token");
-//         //     assert_eq!(result, Ok(("", ArgumentValue::Token("token"))));
-//         // }
+        // #[test]
+        // fn succeeds_when_there_is_no_remainder() {
+        //     let result = argument_value("token");
+        //     assert_eq!(result, Ok(("", ArgumentValue::Token("token"))));
+        // }
 
-//         mod string_value {
-//             #[cfg(test)]
-//             use super::*;
+        mod string_value {
+            #[cfg(test)]
+            use super::*;
 
-//             #[test]
-//             fn succeeds_when_there_is_a_remainder() {
-//                 assert_eq!(
-//                     argument_value("\"arg_value1\" leftovers"),
-//                     Ok((" leftovers", ArgumentValue::String("arg_value1")))
-//                 );
-//             }
+            #[test]
+            fn succeeds_when_there_is_a_remainder() {
+                assert_eq!(
+                    argument_value("\"arg_value1\" leftovers"),
+                    Ok((
+                        " leftovers",
+                        ArgumentValue::String("arg_value1".to_string())
+                    ))
+                );
+            }
 
-//             #[test]
-//             fn succeeds_when_there_is_no_remainder() {
-//                 assert_eq!(
-//                     argument_value("\"arg_value2\""),
-//                     Ok(("", ArgumentValue::String("arg_value2")))
-//                 );
-//             }
+            #[test]
+            fn succeeds_when_there_is_no_remainder() {
+                assert_eq!(
+                    argument_value("\"arg_value2\""),
+                    Ok(("", ArgumentValue::String("arg_value2".to_string())))
+                );
+            }
 
-//             #[test]
-//             fn fails_when_there_is_no_closing_quote() {
-//                 assert_eq!(
-//                     argument_value("\"arg_value2"),
-//                     Err(nom::Err::Incomplete(nom::Needed::Size(1)))
-//                 );
-//             }
-//         }
+            #[test]
+            fn fails_when_there_is_no_closing_quote() {
+                assert_eq!(
+                    argument_value("\"arg_value2"),
+                    Err(nom::Err::Incomplete(nom::Needed::Size(1)))
+                );
+            }
+        }
 
-//         mod token_value {
-//             #[cfg(test)]
-//             use super::*;
+        mod token_value {
+            #[cfg(test)]
+            use super::*;
 
-//             #[test]
-//             fn succeeds_when_there_is_a_remainder() {
-//                 let result = argument_value("stdout leftovers");
-//                 assert_eq!(result, Ok((" leftovers", ArgumentValue::Token("stdout"))));
-//             }
+            #[test]
+            fn succeeds_when_there_is_a_remainder() {
+                let result = argument_value("stdout leftovers");
+                assert_eq!(
+                    result,
+                    Ok((" leftovers", ArgumentValue::Token("stdout".to_string())))
+                );
+            }
 
-//             // #[test]
-//             // fn succeeds_when_there_is_no_remainder() {
-//             //     let result = argument_value("stderr");
-//             //     assert_eq!(result, Ok(("", ArgumentValue::Token("stderr"))));
-//             // }
-//         }
-//     }
-// }
+            // #[test]
+            // fn succeeds_when_there_is_no_remainder() {
+            //     let result = argument_value("stderr");
+            //     assert_eq!(result, Ok(("", ArgumentValue::Token("stderr"))));
+            // }
+        }
+    }
+}
