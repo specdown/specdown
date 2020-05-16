2a17c91fee59da07789b47353405d97937c0ae99
Tom Oram <me@tomoram.io>
2020-05-16 20:00:47 +0100

    Remove commented test

--- src/parser/actions.rs
+++ src/parser/actions.rs
@@ -33,22 +33,6 @@ mod tests {
         );
     }
 
-    // #[test]
-    // fn create_action_for_script_with_spaces_in_info() {
-    //     assert_eq!(
-    //         create_action(
-    //             "zsh , script ( name = \"spaced-script\" )".to_string(),
-    //             "spaced code".to_string()
-    //         ),
-    //         Ok(
-    //             Action::Script(
-    //                 ScriptName("spaced-script".to_string()),
-    //                 ScriptCode("spaced code".to_string())
-    //             )
-    //         ) as Result<Action>
-    //     );
-    // }
-
     #[test]
     fn create_action_for_verify() {
         assert_eq!(
