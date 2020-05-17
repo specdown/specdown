07f760084b51cc4d54400aede4b6a566afb6e357
Tom Oram <me@tomoram.io>
2020-05-17 19:28:34 +0100

    Separate markdown parsing and creating actions

---
+++ src/parser/markdown.rs
@@ -0,0 +1,100 @@
+use comrak::nodes::{AstNode, NodeCodeBlock, NodeValue};
+use comrak::{parse_document, Arena, ComrakOptions};
+
+use super::error::{Error, Result};
+
+#[derive(Debug, PartialEq)]
+pub enum Element {
+    FencedCodeBlock { info: String, literal: String },
+}
+
+pub fn parse(markdown: &str) -> Result<Vec<Element>> {
+    let arena = Arena::new();
+    let root = parse_document(&arena, &markdown, &ComrakOptions::default());
+    extract_elements(root)
+}
+
+fn extract_elements<'a>(root: &'a AstNode<'a>) -> Result<Vec<Element>> {
+    let node_value = &root.data.borrow_mut().value;
+
+    match node_value {
+        NodeValue::Document => Ok(root.children()),
+        _ => Err(Error::RootMustBeDocument),
+    }?
+    .filter_map(to_element)
+    .collect()
+}
+
+fn to_element<'a>(node: &'a AstNode<'a>) -> Option<Result<Element>> {
+    match node.data.borrow().value.clone() {
+        NodeValue::CodeBlock(block) => Some(to_fenced_code_block_element(&block)),
+        _ => None,
+    }
+}
+
+fn to_fenced_code_block_element(block: &NodeCodeBlock) -> Result<Element> {
+    let (info, literal) = node_block_to_components(&block)?;
+    let element = Element::FencedCodeBlock { info, literal };
+    Ok(element)
+}
+
+fn node_block_to_components(block: &NodeCodeBlock) -> Result<(String, String)> {
+    let info = char_vec_to_string(&block.info)?;
+    let literal = char_vec_to_string(&block.literal)?;
+
+    Ok((info, literal))
+}
+
+fn char_vec_to_string(chars: &[u8]) -> Result<String> {
+    match String::from_utf8(chars.to_vec()) {
+        Ok(string) => Ok(string),
+        Err(err) => Err(Error::StringEncodingFailed(err.to_string())),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use indoc::indoc;
+
+    #[test]
+    fn no_actions_returned_when_not_code_blocks_in_markdown() {
+        let markdown = indoc!("# This is markdown");
+
+        assert_eq!(parse(markdown), Ok(vec![]));
+    }
+
+    #[test]
+    fn fenced_block_quotes_are_returned_when_the_exist_in_the_markdown() {
+        let markdown = indoc!(
+            "# This is markdown
+            
+            ```info1
+            literal1
+            ```
+            
+            content
+
+            ```info2
+            literal2
+            ```
+
+            footer
+            "
+        );
+
+        assert_eq!(
+            parse(markdown),
+            Ok(vec![
+                Element::FencedCodeBlock {
+                    info: "info1".to_string(),
+                    literal: "literal1\n".to_string(),
+                },
+                Element::FencedCodeBlock {
+                    info: "info2".to_string(),
+                    literal: "literal2\n".to_string(),
+                },
+            ])
+        );
+    }
+}
--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,57 +1,24 @@
-use comrak::nodes::{AstNode, NodeCodeBlock, NodeValue};
-use comrak::{parse_document, Arena, ComrakOptions};
-
 use crate::types::Action;
 
 mod actions;
 mod code_block_info;
 mod error;
 mod function_string;
+mod markdown;
 
-use error::{Error, Result};
+use error::Result;
 
 pub fn parse(markdown: &str) -> Result<Vec<Action>> {
-    let arena = Arena::new();
-    let root = parse_document(&arena, &markdown, &ComrakOptions::default());
-    let actions = extract_actions(root)?;
+    let elements = markdown::parse(markdown)?;
 
-    Ok(actions)
+    elements.iter().map(to_action).collect()
 }
 
-fn extract_actions<'a>(root: &'a AstNode<'a>) -> Result<Vec<Action>> {
-    let node_value = &root.data.borrow_mut().value;
-
-    match node_value {
-        NodeValue::Document => Ok(root.children()),
-        _ => Err(Error::RootMustBeDocument),
-    }?
-    .filter_map(|node| to_codeblock(node).map(|block| to_action(&block)))
-    .collect()
-}
-
-fn to_codeblock<'a>(node: &'a AstNode<'a>) -> Option<NodeCodeBlock> {
-    match node.data.borrow().value.clone() {
-        NodeValue::CodeBlock(block) => Some(block),
-        _ => None,
-    }
-}
-
-fn to_action(block: &NodeCodeBlock) -> Result<Action> {
-    let (info, literal) = node_block_to_components(&block)?;
-    actions::create_action(&info, literal)
-}
-
-fn node_block_to_components(block: &NodeCodeBlock) -> Result<(String, String)> {
-    let info = char_vec_to_string(&block.info)?;
-    let literal = char_vec_to_string(&block.literal)?;
-
-    Ok((info, literal))
-}
-
-fn char_vec_to_string(chars: &[u8]) -> Result<String> {
-    match String::from_utf8(chars.to_vec()) {
-        Ok(string) => Ok(string),
-        Err(err) => Err(Error::StringEncodingFailed(err.to_string())),
+fn to_action(element: &markdown::Element) -> Result<Action> {
+    match element {
+        markdown::Element::FencedCodeBlock { info, literal } => {
+            actions::create_action(&info, literal.clone())
+        }
     }
 }
 
