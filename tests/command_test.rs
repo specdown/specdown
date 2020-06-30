77964d021e6e32df67e655be78a19eb848f1ed8a
GitHub <noreply@github.com>
2020-06-30 19:24:10 +0000

    Release v0.32.0

--- tests/command_test.rs
+++ tests/command_test.rs
@@ -129,7 +129,7 @@ fn test_displays_error_when_required_args_are_missing() {
         .failure()
         .stderr(indoc!(
             "
-            specdown 0.31.0
+            specdown 0.32.0
             A tool to test markdown files and drive devlopment from documentation.
             
             USAGE:
