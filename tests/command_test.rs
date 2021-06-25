7fdaa0e598114a89a8cb7af0c88365535e482189
Versio <github.com/chaaz/versio>
2021-06-25 22:21:10 +0000

    build(deploy): Versio update versions

--- tests/command_test.rs
+++ tests/command_test.rs
@@ -190,7 +190,7 @@ fn test_displays_error_when_required_args_are_missing() {
         .failure()
         .stderr(formatdoc!(
             "
-            specdown 0.44.0
+            specdown 0.45.0
             A tool to test markdown files and drive devlopment from documentation.
 
             USAGE:
