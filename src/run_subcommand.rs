2fb0f63e8e225b6c38ad904074c26ff34e33095c
Tom Oram <me@tomoram.io>
2021-06-26 20:40:41 +0100

    refactor: Use absolute paths when reading files

--- src/run_subcommand.rs
+++ src/run_subcommand.rs
@@ -1,6 +1,6 @@
 use clap::{Arg, SubCommand};
 use std::fs;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 use crate::parser;
 use crate::results::basic_printer::BasicPrinter;
@@ -53,15 +53,16 @@ fn execute_run(spec_files: &[&Path], shell_cmd: &str, running_dir: Option<&Path>
     let printer: Box<dyn Printer> = Box::new(BasicPrinter::new());
     let spec_dir = std::env::current_dir().expect("Failed to get current working directory");
 
+    if let Some(dir) = running_dir {
+        fs::create_dir_all(dir).expect("Failed to create running directory");
+        std::env::set_current_dir(dir).expect("Failed to set running directory");
+    }
+
     for spec_file in spec_files {
         printer.print_spec_file(spec_file);
-        let contents = fs::read_to_string(spec_file).expect("failed to read spec file");
+        let contents = fs::read_to_string(to_absolute(spec_file, &spec_dir))
+            .expect("failed to read spec file");
         let actions = parser::parse(&contents);
-
-        if let Some(dir) = running_dir {
-            fs::create_dir_all(dir).expect("Failed to create running directory");
-            std::env::set_current_dir(dir).expect("Failed to set running directory");
-        }
 
         match actions {
             Ok(action_list) => run_actions(&action_list, shell_cmd, &*printer),
@@ -72,8 +73,50 @@ fn execute_run(spec_files: &[&Path], shell_cmd: &str, running_dir: Option<&Path>
                 std::process::exit(1)
             }
         }
+    }
+}
 
-        std::env::set_current_dir(spec_dir.as_path())
-            .expect("Failed to return to the spec directory");
+fn to_absolute(path: &Path, working_dir: &Path) -> PathBuf {
+    if path.has_root() {
+        path.to_path_buf()
+    } else {
+        working_dir.join(path)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::to_absolute;
+
+    mod to_absolute {
+        use super::to_absolute;
+        use std::path::Path;
+
+        #[cfg(not(windows))]
+        #[test]
+        fn test_returns_the_path_when_it_is_absolute() {
+            let path = Path::new("/home/user/file");
+            let working_dir = Path::new("/var");
+            assert_eq!(path, to_absolute(path, working_dir));
+        }
+
+        #[cfg(not(windows))]
+        #[test]
+        fn test_returns_the_working_dir_prepended_when_path_is_relative() {
+            let path = Path::new("./file");
+            let working_dir = Path::new("/var");
+            assert_eq!(Path::new("/var/file"), to_absolute(path, working_dir));
+        }
+
+        #[cfg(not(windows))]
+        #[test]
+        fn test_returns_the_working_dir_prepended_when_path_contains_parent() {
+            let path = Path::new("../file");
+            let working_dir = Path::new("/var/lib");
+            assert_eq!(
+                Path::new("/var/lib/../file"),
+                to_absolute(path, working_dir)
+            );
+        }
     }
 }
