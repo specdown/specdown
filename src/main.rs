9510de5acb248400a4f9d7015b1a52a86fc4a40f
Tom Oram <me@tomoram.io>
2021-06-26 20:47:01 +0100

    refactor: Move commands into module

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,27 @@ extern crate nom;
 
 use clap::{App, AppSettings};
 
+mod commands;
 mod parser;
 mod results;
-mod run_subcommand;
 mod runner;
-mod strip_subcommand;
 mod types;
 
 fn main() {
     let app = App::new("specdown")
         .about("A tool to test markdown files and drive devlopment from documentation.")
         .version(env!("CARGO_PKG_VERSION"))
-        .subcommand(run_subcommand::create())
-        .subcommand(strip_subcommand::create())
+        .subcommand(commands::run::create())
+        .subcommand(commands::strip::create())
         .setting(AppSettings::ArgRequiredElseHelp);
 
     let matches = app.get_matches();
 
-    if matches.is_present(run_subcommand::NAME) {
-        let run_matches = matches.subcommand_matches(run_subcommand::NAME).unwrap();
-        run_subcommand::execute(run_matches);
-    } else if matches.is_present(strip_subcommand::NAME) {
-        let strip_matches = matches.subcommand_matches(strip_subcommand::NAME).unwrap();
-        strip_subcommand::execute(strip_matches);
+    if matches.is_present(commands::run::NAME) {
+        let run_matches = matches.subcommand_matches(commands::run::NAME).unwrap();
+        commands::run::execute(run_matches);
+    } else if matches.is_present(commands::strip::NAME) {
+        let strip_matches = matches.subcommand_matches(commands::strip::NAME).unwrap();
+        commands::strip::execute(strip_matches);
     }
 }
