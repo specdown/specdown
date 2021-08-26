49b8bf618201af02928ed363e5a39fd41b3e3a72
Tom Oram <me@tomoram.io>
2021-08-26 20:37:59 +0100

    refactor: Tidy up BasicPrinter

--- src/results/basic_printer.rs
+++ src/results/basic_printer.rs
@@ -6,7 +6,7 @@ use super::action_result::ActionResult;
 use super::printer::Printer;
 use crate::runner::error::Error;
 use crate::runner::RunEvent;
-use crate::types::{ScriptAction, ScriptName, Source, Stream, VerifyAction};
+use crate::types::{ScriptAction, Stream, VerifyAction};
 
 struct Summary {
     pub number_succeeded: u32,
@@ -54,115 +54,31 @@ impl BasicPrinter {
     }
 
     fn print_result(&mut self, result: &ActionResult) {
-        match result {
-            ActionResult::Script {
-                action:
-                    ScriptAction {
-                        script_name: ScriptName(name),
-                        expected_exit_code,
-                        ..
-                    },
-                exit_code,
-                stdout,
-                stderr,
-                ..
-            } => {
-                let message = if result.success() {
-                    "succeeded".to_string()
-                } else {
-                    let expected =
-                        expected_exit_code.map_or("None".to_string(), |code| code.into());
-                    let got = exit_code.map_or("None".to_string(), |code| code.to_string());
-
-                    format!("failed (expected exitcode {}, got {})", expected, got)
-                };
-
-                if result.success() {
-                    self.summary.number_succeeded += 1;
-                } else {
-                    self.summary.number_failed += 1;
-                }
-
-                let full_message = &format!("  - script '{}' {}", name, message);
-                if result.success() {
-                    self.display_success(full_message);
-                } else {
-                    self.display_error(full_message);
-                    self.display(&format!(
-                        "\n=== stdout:\n{}\n\n=== stderr:\n{}\n\n",
-                        stdout, stderr
-                    ));
-                }
-            }
-            ActionResult::Verify { action, got, .. } => {
-                let VerifyAction {
-                    source:
-                        Source {
-                            name: script_name,
-                            stream,
-                        },
-                    expected_value: expected,
-                } = action;
-                let message = &format!(
-                    "  - verify {} from '{}' {}",
-                    stream_to_string(stream),
-                    String::from(script_name.clone()),
-                    if result.success() {
-                        "succeeded"
-                    } else {
-                        "failed"
-                    }
-                );
-
-                if result.success() {
-                    self.summary.number_succeeded += 1;
-                } else {
-                    self.summary.number_failed += 1;
-                }
-
-                if result.success() {
-                    self.display_success(message);
-                } else {
-                    self.display_error(message);
-                    self.display(&format!(
-                        "===\n{}\n===",
-                        colored_diff::PrettyDifference {
-                            expected: &String::from(expected.clone()),
-                            actual: got,
-                        }
-                    ));
-                }
-            }
-            ActionResult::CreateFile { action, .. } => {
-                self.summary.number_succeeded += 1;
-                self.display_success(&format!(
-                    "  - file {} created",
-                    String::from(action.clone().file_path)
-                ));
-            }
-        }
+        self.count_action(result);
+        self.display_action(result);
+        self.dsiplay_action_error(result);
     }
 
     fn print_error(&self, error: &Error) {
-        match error {
+        self.display_error_item(&match error {
             Error::ScriptOutputMissing {
                 missing_script_name,
             } => {
-                self.display_error(&format!(
-                    "  - Failed to verify the output of '{}': No script with that name has been executed yet.",
+                format!(
+                    "Failed to verify the output of '{}': No script with that name has been executed yet.",
                     missing_script_name
-                ));
-            }
-            Error::CommandFailed { command, message } => self.display_error(&format!(
-                "  - Failed to run command: {} (Error: {})",
+                )
+            },
+            Error::CommandFailed { command, message } => format!(
+                "Failed to run command: {} (Error: {})",
                 command, message
-            )),
-            Error::BadShellCommand { command, message } => self.display_error(&format!(
-                "  - Invalid shell command provided: {} (Error: {})",
+            ),
+            Error::BadShellCommand { command, message } => format!(
+                "Invalid shell command provided: {} (Error: {})",
                 command, message
-            )),
-            Error::RunFailed { message } => self.display_error(&format!("  - {}", message)),
-        }
+            ),
+            Error::RunFailed { message } => message.to_string(),
+        });
     }
 
     fn print_summary(&self) {
@@ -174,9 +90,112 @@ impl BasicPrinter {
         ));
     }
 
+    fn display_action(&mut self, result: &ActionResult) {
+        let title = BasicPrinter::action_title(result);
+        let result_message = BasicPrinter::action_result_message(result);
+        let full_message = &format!("{} {}", title, result_message);
+        if result.success() {
+            self.display_success_item(full_message);
+        } else {
+            self.display_error_item(full_message);
+        }
+    }
+
+    fn action_title(result: &ActionResult) -> String {
+        match result {
+            ActionResult::Script { action, .. } => {
+                format!("script '{}'", String::from(action.script_name.clone()))
+            }
+            ActionResult::Verify { action, .. } => format!(
+                "verify {} from '{}'",
+                stream_to_string(&action.source.stream),
+                String::from(action.source.name.clone()),
+            ),
+            ActionResult::CreateFile { action, .. } => {
+                format!("file {}", String::from(action.file_path.clone()))
+            }
+        }
+    }
+
+    fn count_action(&mut self, result: &ActionResult) {
+        if result.success() {
+            self.summary.number_succeeded += 1;
+        } else {
+            self.summary.number_failed += 1;
+        }
+    }
+
+    fn action_result_message(result: &ActionResult) -> String {
+        match result {
+            ActionResult::Script {
+                action: ScriptAction {
+                    expected_exit_code, ..
+                },
+                exit_code,
+                ..
+            } => {
+                if result.success() {
+                    "succeeded".to_string()
+                } else {
+                    let expected =
+                        expected_exit_code.map_or("None".to_string(), |code| code.into());
+                    let got = exit_code.map_or("None".to_string(), |code| code.to_string());
+
+                    format!("failed (expected exitcode {}, got {})", expected, got)
+                }
+            }
+            ActionResult::Verify { .. } => {
+                if result.success() {
+                    "succeeded".to_string()
+                } else {
+                    "failed".to_string()
+                }
+            }
+            ActionResult::CreateFile { .. } => "created".to_string(),
+        }
+    }
+
+    fn dsiplay_action_error(&mut self, result: &ActionResult) {
+        match result {
+            ActionResult::Script { stdout, stderr, .. } => {
+                if !result.success() {
+                    self.display(&format!(
+                        "\n=== stdout:\n{}\n\n=== stderr:\n{}\n\n",
+                        stdout, stderr
+                    ));
+                }
+            }
+            ActionResult::Verify { action, got, .. } => {
+                let VerifyAction {
+                    expected_value: expected,
+                    ..
+                } = action;
+
+                if !result.success() {
+                    self.display(&format!(
+                        "===\n{}\n===",
+                        colored_diff::PrettyDifference {
+                            expected: &String::from(expected.clone()),
+                            actual: got,
+                        }
+                    ));
+                }
+            }
+            ActionResult::CreateFile { .. } => {}
+        }
+    }
+
     fn display(&self, text: &str) {
         let display = &self.display_function;
         display(text);
+    }
+
+    fn display_success_item(&self, text: &str) {
+        self.display_success(&format!("  - {}", text));
+    }
+
+    fn display_error_item(&self, text: &str) {
+        self.display_error(&format!("  - {}", text));
     }
 
     fn display_success(&self, text: &str) {
