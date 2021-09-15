365158148d1857da63eca1fc586dad0b2cc3ef2d
Tom Oram <me@tomoram.io>
2021-09-15 16:14:03 +0100

    refactor: Inline unnecessary function

--- src/commands/strip.rs
+++ src/commands/strip.rs
@@ -22,10 +22,6 @@ pub fn execute(run_matches: &clap::ArgMatches<'_>) {
         .map(Path::new)
         .expect("spec-file should always exist");
 
-    execute_strip(spec_file);
-}
-
-fn execute_strip(spec_file: &Path) {
     let contents = fs::read_to_string(spec_file).expect("failed to read spec file");
     let stripped = parser::strip(&contents);
     println!("{}", stripped);
