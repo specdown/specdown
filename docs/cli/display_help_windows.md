04990076e70901eb28b0c71fff8faf144a73cfad
Versio <github.com/chaaz/versio>
2022-01-10 06:22:45 +0000

    build(deploy): Versio update versions

--- docs/cli/display_help_windows.md
+++ docs/cli/display_help_windows.md
@@ -9,7 +9,7 @@ specdown
 Outputs:
 
 ```,verify(stream=stderr)
-specdown 1.1.3
+specdown 1.1.4
 A tool to test markdown files and drive devlopment from documentation.
 
 USAGE:
--- tests/command_test.rs
+++ tests/command_test.rs
@@ -251,7 +251,7 @@ fn test_displays_error_when_required_args_are_missing() {
         .failure()
         .stderr(formatdoc!(
             "
-            specdown 1.1.3
+            specdown 1.1.4
             A tool to test markdown files and drive devlopment from documentation.
 
             USAGE:
